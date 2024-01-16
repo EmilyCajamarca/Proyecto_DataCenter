2cf9239e0b0501643e155c2d6cb684007698c806
EmilyCajamarca <emily.cajamarca@ucuenca.edu.ec>
2024-01-16 13:09:10 -0500

    edad

--- my-app/controllers/funciones_login.py
+++ my-app/controllers/funciones_login.py
@@ -11,7 +11,7 @@ import re
 from werkzeug.security import generate_password_hash
 
 
-def recibeInsertRegisterUser(cedula, name, surname, id_area, id_rol, pass_user, edad):
+def recibeInsertRegisterUser(cedula, name, surname, id_area, id_rol, pass_user, Edad ):
     respuestaValidar = validarDataRegisterLogin(
         cedula, name, surname, pass_user)
 
@@ -21,10 +21,10 @@ def recibeInsertRegisterUser(cedula, name, surname, id_area, id_rol, pass_user, 
             with connectionBD() as conexion_MySQLdb:
                 with conexion_MySQLdb.cursor(dictionary=True) as mycursor:
                     sql = """
-                    INSERT INTO usuarios(cedula, nombre_usuario, apellido_usuario, id_area, id_rol, password, edad) 
+                    INSERT INTO usuarios(cedula, nombre_usuario, apellido_usuario, id_area, id_rol, password, Edad ) 
                     VALUES (%s, %s, %s, %s, %s, %s, %s)
                     """
-                    valores = (cedula, name, surname, id_area, id_rol, nueva_password, edad)
+                    valores = (cedula, name, surname, id_area, id_rol, nueva_password, Edad )
                     mycursor.execute(sql, valores)
                     conexion_MySQLdb.commit()
                     resultado_insert = mycursor.rowcount
@@ -64,7 +64,7 @@ def info_perfil_session(id):
     try:
         with connectionBD() as conexion_MySQLdb:
             with conexion_MySQLdb.cursor(dictionary=True) as cursor:
-                querySQL = "SELECT id_usuario, nombre_usuario, apellido_usuario, cedula, id_area, id_rol, edad FROM usuarios WHERE id_usuario = %s"
+                querySQL = "SELECT id_usuario, nombre_usuario, apellido_usuario, cedula, id_area, id_rol, Edad  FROM usuarios WHERE id_usuario = %s"
                 cursor.execute(querySQL, (id,))
                 info_perfil = cursor.fetchall()
         return info_perfil
@@ -81,7 +81,7 @@ def procesar_update_perfil(data_form,id):
     apellido_usuario = data_form['surname']
     id_area = data_form['selectArea']
     id_rol= data_form['selectRol']
-    edad= data_form['edad']
+    Edad = data_form['Edad ']
     new_pass_user = data_form['new_pass_user']
     
 
@@ -99,11 +99,11 @@ def procesar_update_perfil(data_form,id):
                             id_area = %s,
                             id_rol = %s,
                             password = %s,
-                            edad = %s
+                            Edad  = %s
                         WHERE id_usuario = %s
                     """
                     params = (nombre_usuario,apellido_usuario, id_area, id_rol,
-                                nueva_password, edad, id_user)
+                                nueva_password, Edad , id_user)
                     cursor.execute(querySQL, params)
                     conexion_MySQLdb.commit()
             return 1
@@ -118,7 +118,7 @@ def procesar_update_perfil(data_form,id):
     print(id_area+" HOLA "+id_rol)
 
     if not pass_actual and not new_pass_user and not repetir_pass_user:
-            return updatePefilSinPass(id_user, nombre_usuario, apellido_usuario, id_area, id_rol, edad )
+            return updatePefilSinPass(id_user, nombre_usuario, apellido_usuario, id_area, id_rol, Edad  )
 
     with connectionBD() as conexion_MySQLdb:
         with conexion_MySQLdb.cursor(dictionary=True) as cursor:
@@ -144,11 +144,11 @@ def procesar_update_perfil(data_form,id):
                                                 apellido_usuario = %s,
                                                 id_area = %s,
                                                 password = %s,
-                                                edad = %s
+                                                Edad  = %s
                                             WHERE id_usuario = %s
                                         """
                                         params = (nombre_usuario,apellido_usuario, id_area,
-                                                  nueva_password, edad, id_user)
+                                                  nueva_password, Edad , id_user)
                                         cursor.execute(querySQL, params)
                                         conexion_MySQLdb.commit()
                                 return cursor.rowcount or []
@@ -161,7 +161,7 @@ def procesar_update_perfil(data_form,id):
 
 
 
-def updatePefilSinPass(id_user, nombre_usuario, apellido_usuario, id_area, id_rol,edad):
+def updatePefilSinPass(id_user, nombre_usuario, apellido_usuario, id_area, id_rol,Edad ):
     try:
         with connectionBD() as conexion_MySQLdb:
             with conexion_MySQLdb.cursor(dictionary=True) as cursor:
@@ -172,10 +172,10 @@ def updatePefilSinPass(id_user, nombre_usuario, apellido_usuario, id_area, id_ro
                         apellido_usuario = %s,
                         id_area = %s,
                         id_rol = %s,
-                        edad = %s
+                        Edad  = %s
                     WHERE id_usuario = %s
                 """
-                params = ( nombre_usuario, apellido_usuario, id_area, id_rol, edad, id_user)
+                params = ( nombre_usuario, apellido_usuario, id_area, id_rol, Edad , id_user)
                 cursor.execute(querySQL, params)
                 conexion_MySQLdb.commit()
         return cursor.rowcount
--- my-app/routers/router_login.py
+++ my-app/routers/router_login.py
@@ -56,11 +56,11 @@ def cpanelRegisterUserBD():
         id_area = request.form['selectArea']
         id_rol = request.form['selectRol']
         pass_user = request.form['pass_user']
-        Edad = request.form['Edad']
+        Edad  = request.form['Edad ']
 
 
         resultData = recibeInsertRegisterUser(
-            cedula, name, surname, id_area,id_rol,pass_user,Edad)
+            cedula, name, surname, id_area,id_rol,pass_user,Edad )
         if (resultData != 0):
             flash('la cuenta fue creada correctamente.', 'success')
             return redirect(url_for('inicio'))
